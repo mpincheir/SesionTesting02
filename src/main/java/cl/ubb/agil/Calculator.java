bb288b71349e819dac753ff4c6b49e271dc8f3f9
Miguel Pincheira <mpincheir@ubiobio.cl>
2016-04-12 18:00:51 -0300

    Calculator Class

---
+++ src/main/java/cl/ubb/agil/Calculator.java
@@ -0,0 +1,25 @@
+package cl.ubb.agil;
+
+public class Calculator {
+
+	public int adder(int num_1, int num_2){
+		return num_1+num_2;		
+	}
+	
+	public int subtractor(int num_1, int num_2){
+		return num_1-num_2;		
+	}
+	
+	public int basicExpressionSolver(String expression){
+		int c=0;
+		return c;
+	}
+	
+	public float divider(int number, int divider){
+		return (number/divider);		
+	}
+	
+	public int multiplier(int number, int factor){
+		return (number * factor);		
+	}
+}
